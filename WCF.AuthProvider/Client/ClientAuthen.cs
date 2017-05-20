0889f0ce53182e0023de25650870298d768eac5a
bi7prk <im@bi7prk.com>
2017-05-20 12:07:58 +0800

    添加注释

--- WCF.AuthProvider/Client/ClientAuthen.cs
+++ WCF.AuthProvider/Client/ClientAuthen.cs
@@ -11,7 +11,7 @@ namespace WCF.AuthProvider.Client
         /// <summary>
         /// 在本地保存已通过认证的用户
         /// </summary>
-        /// <param name="userName"></param>
+        /// <param name="userName">登陆用户的唯一名称，默认为用户ID</param>
         /// <param name="authKey"></param>
         /// <param name="ns"></param>
         public ClientAuthen(string userName, string authKey, string ns= "http://www.bi7prk.com/")
